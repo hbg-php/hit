bc7ab0918e045416499a63ba1df4184dfb13eb0e
Hugo Matheus Freire de Lima <hugo.lima@cnj.jus.br>
2024-12-27 22:00:30 -0300

    fix: fixing format

--- src/ValueObjects/Issue.php
+++ src/ValueObjects/Issue.php
@@ -2,7 +2,17 @@
 
 namespace Hit\ValueObjects;
 
-class Issue
+/**
+ * @internal This class is not meant to be used outside.
+ */
+final readonly class Issue
 {
-
+    /**
+     * Issue constructor.
+     */
+    public function __construct(
+        public Misspelling $misspelling,
+        public string $file,
+        public int $line,
+    ) {}
 }
--- src/ValueObjects/Misspelling.php
+++ src/ValueObjects/Misspelling.php
@@ -2,7 +2,18 @@
 
 namespace Hit\ValueObjects;
 
-class Misspelling
+/**
+ * @internal This class is not meant to be used outside.
+ */
+final readonly class Misspelling
 {
-
+    /**
+     * Misspelling constructor.
+     *
+     * @param  array<int, string>  $suggestions
+     */
+    public function __construct(
+        public string $word,
+        public array $suggestions
+    ) {}
 }
