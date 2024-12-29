aacad1bba558032036fed1b59ae4ac64adcb8648
Hugo Matheus Freire de Lima <hugo.lima@cnj.jus.br>
2024-12-29 16:15:11 -0300

    some fixing

--- src/Console/Commands/HitCommand.php
+++ src/Console/Commands/HitCommand.php
@@ -12,6 +12,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use function Termwind\render;
 use function Termwind\renderUsing;
 
 /**
