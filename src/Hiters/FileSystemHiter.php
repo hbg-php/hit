3f5c00d19fa114a1204191b630528999915ed274
Hugo Matheus Freire de Lima <hugo.lima@cnj.jus.br>
2024-12-30 09:36:22 -0300

    fix

--- src/Hiters/FileSystemHiter.php
+++ src/Hiters/FileSystemHiter.php
@@ -7,6 +7,9 @@ namespace Hit\Hiters;
 use Hit\Config;
 use Hit\Contracts\Hiter;
 use Hit\Contracts\Hithunter;
+use Hit\ValueObjects\Issue;
+use Hit\ValueObjects\Misspelling;
+use Symfony\Component\Finder\Finder;
 
 /**
  * @internal
@@ -25,6 +28,35 @@ readonly class FileSystemHiter implements Hiter
      */
     public function hit(array $parameters): array
     {
-        return [];
+        $filesOrDirectories = Finder::create()
+            ->notPath($this->config->whitelistedDirectories)
+            ->ignoreDotFiles(true)
+            ->ignoreVCS(true)
+            ->ignoreUnreadableDirs()
+            ->ignoreVCSIgnored(true)
+            ->in($parameters['directory']);
+
+        $issues = [];
+
+        foreach ($filesOrDirectories as $filesOrDirectory) {
+            $name = $filesOrDirectory->getFilenameWithoutExtension();
+            $name = strtolower((string) preg_replace('/(?<!^)[A-Z]/', ' $0', $name));
+
+            $issues = [
+                ...$issues,
+                ...array_map(
+                    fn (Misspelling $misspelling): Issue => new Issue(
+                        $misspelling,
+                        $filesOrDirectory->getRealPath(),
+                        0,
+                    ),
+                    $this->hithunter->hit($name),
+                ),
+            ];
+        }
+
+        usort($issues, fn (Issue $a, Issue $b): int => $a->file <=> $b->file);
+
+        return array_values($issues);
     }
 }
--- src/Services/InMemoryHitHunter.php
+++ src/Services/InMemoryHitHunter.php
@@ -6,6 +6,8 @@ namespace Hit\Services;
 
 use Hit\Config;
 use Hit\Contracts\Hithunter;
+use Hit\ValueObjects\Misspelling;
+use PhpSpellcheck\MisspellingInterface;
 use PhpSpellcheck\Spellchecker\Aspell;
 
 final readonly class InMemoryHitHunter implements Hithunter
@@ -13,8 +15,7 @@ final readonly class InMemoryHitHunter implements Hithunter
     public function __construct(
         private Config $config,
         private Aspell $aspell
-    )
-    {
+    ) {
         //
     }
 
@@ -28,6 +29,25 @@ final readonly class InMemoryHitHunter implements Hithunter
 
     public function hit(string $text): array
     {
-        return [];
+        $misspellings = $this->filterWhiteListedWords(iterator_to_array($this->aspell->check($text)));
+
+        return array_map(fn (MisspellingInterface $misspelling): Misspelling => new Misspelling(
+            $misspelling->getWord(),
+            array_slice($misspelling->getSuggestions(), 0, 4),
+        ), $misspellings);
+    }
+
+    /**
+     * Filter out misspellings that are in the white list.
+     *
+     * @param  array<int, MisspellingInterface>  $misspellings
+     * @return array<int, MisspellingInterface> $misspellings
+     */
+    public function filterWhiteListedWords(array $misspellings): array
+    {
+        return array_filter($misspellings, fn (MisspellingInterface $misspelling): bool => ! in_array(
+            strtolower($misspelling->getWord()),
+            $this->config->whitelistedWords
+        ));
     }
 }
